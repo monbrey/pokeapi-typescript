bb8bd64d74423752ed3188303475fe39d14375aa
Monbrey <rsm999@uowmail.edu.au>
2019-08-08 08:55:16 +1000

    JSDoc the endpoint

--- src/classes/Endpoint.ts
+++ src/classes/Endpoint.ts
@@ -2,7 +2,7 @@ import Collection = require("collection");
 import fetch from "node-fetch";
 import { INamedApiResource } from "../interfaces/Utility/CommonModels";
 
-export type EndpointParams = number | string | Array<number | string>;
+export type EndpointParam = number | string | Array<number | string>;
 
 const BASE_URI = "https://pokeapi.co/api/v2";
 
@@ -20,7 +20,12 @@ export default class Endpoint<T> {
         this.nameMap = new Map<string, number>();
     }
 
-    public get(param: EndpointParams): T | T[] {
+    /**
+     * Retrieves an item from the cache by name or ID
+     * @param {EndpointParam} param - The name/ID of the item to get, or Array of names/IDs
+     * @returns {?T|Array<?T>}
+     */
+    public get(param: EndpointParam): T | T[] {
         switch (typeof (param)) {
             case "number":
                 return this.cache.get(param);
@@ -39,11 +44,20 @@ export default class Endpoint<T> {
         }
     }
 
+    /**
+     * Returns all resources currently cached from this endpoint as a Collection
+     * @returns {Collection<number, T>}
+     */
     public getAll(): Collection<number, T> {
         return this.cache;
     }
 
-    public async resolve(param: EndpointParams): Promise<T | T[]> {
+    /**
+     * Gets an item from cache, or attempts to fetch it from the API if no cached resource is available
+     * @param {EndpointParam} param - The name/ID of the item to resolve, or Array of names/IDs
+     * @returns {Promise<T|T[]>}
+     */
+    public async resolve(param: EndpointParam): Promise<T | T[]> {
         switch (typeof (param)) {
             case "number":
                 return this.cache.get(param) || this.fetch(param);
@@ -59,7 +73,13 @@ export default class Endpoint<T> {
         }
     }
 
-    public async fetch(param: EndpointParams): Promise<T | T[]> {
+    /**
+     * Fetches a resource from the API
+     * @param {EndpointParam} param - The name/ID of the item to fetch, or Array of names/IDs
+     * @param {boolean} [cache=true] - Whether or not to cache this resource
+     * @returns {Promise<T|T[]>}
+     */
+    public async fetch(param: EndpointParam, cache: boolean = true): Promise<T | T[]> {
         if (typeof param === "object") {
             return Promise.all(param.map(async p => {
                 const data = await fetch(`${BASE_URI}/${this.resource}/${param}`).then(res => res.json());
@@ -75,7 +95,13 @@ export default class Endpoint<T> {
         }
     }
 
-    public async fetchAll(): Promise<INamedApiResource<T>> {
-        return fetch(`${BASE_URI}/${this.resource}`).then(res => res.json());
-    }
+    // /**
+    //  * Fetches the resource list from the API. Results are not cached.
+    //  * @param {number} [limit=20] - How many resources to fetch
+    //  * @param {offset} [offset=0] - 
+    //  * @returns {Promise<INamedApiResource<T>>}
+    //  */
+    // public async fetchAll(limit = 20, offset?): Promise<INamedApiResource<T>> {
+    //     return fetch(`${BASE_URI}/${this.resource}`).then(res => res.json());
+    // }
 }
